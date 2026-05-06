39bb7cc728c416c384c1bacf5385406510de52aa
Amansundar <snowcoolwhite84@gmail.com>
2026-05-06 12:06:23 +0000

    Refactor unregister function docstring and adjust participant item margin

--- src/app.py
+++ src/app.py
@@ -83,7 +83,7 @@ def get_activities():
 
 @app.post("/activities/{activity_name}/unregister")
 def unregister_from_activity(activity_name: str, email: str):
-    """Unregister a student from an activity"""
+    """Unregister a student"""
     if activity_name not in activities:
         raise HTTPException(status_code=404, detail="Activity not found")
     activity = activities[activity_name]
